34712a349f3f964f5b66ad33abc2e4c578a0f1cc
MohamedMohamed <m01126165016@icloud.com>
2024-07-12 20:40:54 +0300

    done

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,6 +43,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        lifecycleScope.launch {
+            val menuItems = getMenu("Salads")
+
+            runOnUiThread {
+                menuItemsLiveData.value = menuItems
+            }
+        }
+
         setContent {
             LittleLemonTheme {
                 Surface(
@@ -51,11 +60,21 @@ class MainActivity : ComponentActivity() {
                     Column(
                         modifier = Modifier.fillMaxSize()
                     ) {
+                        val items by menuItemsLiveData.observeAsState(emptyList())
+                        MenuItems(items)
                     }
                 }
             }
         }
     }
+
+    private suspend fun getMenu(category: String): List<String> {
+        val response: Map<String, MenuCategory> =
+            client.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
+                .body()
+        return response[category]?.menu ?: listOf()
+    }
+
 }
 
 @Composable
